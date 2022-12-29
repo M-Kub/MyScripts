88016e732e66756b02aa99f10a3fc759d746376e
M-Kub <mkubesch@gmail.com>
2022-12-29 13:29:47 +0100

    Added python script for SQL headers

---
+++ python-Scripts/sqlHeaders.py
@@ -0,0 +1,21 @@
+import csv
+import redline
+import sys
+
+readline.set_completer_delims(" \t\n=")
+readline.parse_and_bind("tab: complete")
+
+def csvOpen(file):
+
+    with open(file, 'r', newline='', enconding='utf-8-sig') as csvfile:
+        reader = csv.reader(csvfile)
+        i = next(reader)
+        dRows = sum(1 for line in reader)
+        print(f"You have {dRows} rows in the CSV file:\n" f"Your headers are:\n {i}
+
+
+if len(sys.argv) > 1:
+    file = sys.argv[1]
+else:
+    file = input("Please enter the filename to show the CSV headers: ")
+csvOpen(file)
